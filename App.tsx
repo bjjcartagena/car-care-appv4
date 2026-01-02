0feeba3d5dd9a9ff1b5315b42b36e7b47b375bb2
bjjcartagena <juanjsct@gmail.com>
2026-01-02 23:40:00 +0100

    Eliminar referencia a VehicleProfileSetup

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
-import VehicleProfileSetup from './pages/VehicleProfileSetup';
 import Dashboard from './pages/Dashboard';
 import TaskDetail from './pages/TaskDetail';
 import Garage from './pages/Garage';
@@ -9,8 +8,8 @@ const App: React.FC = () => {
   return (
     <HashRouter>
       <Routes>
-        <Route path="/" element={<VehicleTypeSelection />} />
-        <Route path="/setup-profile" element={<VehicleProfileSetup />} />
+
+
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/task-detail" element={<TaskDetail />} />
         <Route path="/garage" element={<Garage />} />
